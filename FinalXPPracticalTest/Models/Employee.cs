128b27cdf82f1a03a520679348a3bed811c3d906
hanhndth1808055 <nguyendoanhanh.ulis@gmail.com>
2020-03-17 19:48:08 +0700

    First commit

---
+++ FinalXPPracticalTest/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalXPPracticalTest.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string EmployeeID { get; set; }
+        [Required] 
+        public string EmployeeName { get; set; }
+        [Required] 
+        public string Department { get; set; }
+        [Required] 
+        public int Salary { get; set; }
+    }
+}
